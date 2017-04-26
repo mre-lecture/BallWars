9df05c7cfd408986697e44453fef761db38e3920
Miguel Caballero <Miguel Caballero>
2017-04-27 00:17:13 +0200

    created Gymball and somehow pushed only now the whole VR game

---
+++ Assets/VR-Shooting-Range/Scripts/Entities/Spawnpoint/SpawnPoint.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    private int spawnpointId;
+
+    public void Awake()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.grey;
+    }
+
+    public void OnPhotonPlayerPropertiesChanged(object[] playerAndUpdatedProps)
+    {
+        Hashtable props = (Hashtable)playerAndUpdatedProps[1];
+
+        if (!props.ContainsKey("position"))
+        {
+            return;
+        }
+
+        int position = (int)props["position"];
+
+        if (position != spawnpointId)
+        {
+            return;
+        }
+
+        GetComponent<MeshRenderer>().material.color = FindObjectOfType<ColorPerPlayer>().Colors[spawnpointId];
+    }
+
+    public void OnPhotonPlayerDisconnected(PhotonPlayer player)
+    {
+        if (!player.CustomProperties.ContainsKey("position"))
+        {
+            return;
+        }
+
+        int position = (int)player.CustomProperties["position"];
+
+        if (position != spawnpointId)
+        {
+            return;
+        }
+
+        GetComponent<MeshRenderer>().material.color = Color.grey;
+    }
+
+    public void RestoreDefaults()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.grey;
+    }
+}
---
+++ Assets/VR-Shooting-Range/Scripts/UI/ScoringPresenter.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace ExitGames.SportShooting
+{
+    public class ScoringPresenter : MonoBehaviour
+    {
+        [SerializeField]
+        Text[] _idFields;
+
+        [SerializeField]
+        Text[] _scoreFields;
+
+        private ColorPerPlayer colorPicker;
+
+        void OnEnable()
+        {
+            NetworkController.OnUpdateScore += UpdateScore;
+        }
+
+        void OnDisable()
+        {
+            NetworkController.OnUpdateScore -= UpdateScore;
+        }
+
+        void Start()
+        {
+            colorPicker = FindObjectOfType<ColorPerPlayer>();
+
+            UpdateScore();
+        }
+
+        void UpdateScore()
+        {
+            ClearTable(_idFields);
+            ClearTable(_scoreFields);
+            
+            foreach(PhotonPlayer player in PhotonNetwork.playerList)
+            {
+                object positionObj = player.CustomProperties["position"];
+                if (positionObj != null)
+                {
+                    int position = (int)positionObj;
+                    
+                    _idFields[position].color = colorPicker.Colors[position];
+                    _idFields[position].text = player.CustomProperties["name"].ToString();
+
+                    if (player.IsLocal)
+                    {
+                        _idFields[position].text += " (YOU)";
+                    }
+
+                    _scoreFields[position].color = colorPicker.Colors[position];
+                    _scoreFields[position].text = player.CustomProperties["roundScore"].ToString();
+                }
+            }
+        }
+
+        void ClearTable(Text[] table)
+        {
+            for(int i = 0; i < table.Length; i++)
+            {
+                table[i].text = "";
+            }
+        }
+    }
+}
