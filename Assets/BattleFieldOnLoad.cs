4fffa8e065291efe00e405854dfc0fdabdc3ef86
xrounder Miguel Caballero <1414163@stud.hs-mannheim.de>
2017-05-17 15:05:24 +0200

    added sounds in game and changed build settings
    added sound
    
    fixed missing prefabs in menu
    
    menu sound and scene change sound are playing
    
    added music when playing and changed build settings

---
+++ Assets/BattleFieldOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleFieldOnLoad : MonoBehaviour {
+
+    [SerializeField] private AudioClip game_music;
+
+	// Use this for initialization
+	void Start () {
+        SoundManager.instance.ChangeMusic(game_music);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ViveControllerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerManager : SteamVR_TrackedController {
+
+    public AudioClip throwingSound;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    override
+    public void OnTriggerUnclicked(ClickedEventArgs e)
+    {
+        base.OnTriggerUnclicked(e);
+        SoundManager.instance.RandomizeSfx(throwingSound);
+    }
+}
