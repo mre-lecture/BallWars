8e4284c4df1c6264b28ef6277ff3e76be332a0da
Empea Lappy <andrej@empeaarvr.com>
2017-05-24 15:31:38 +0200

    Added Balldespawner

---
+++ Assets/BallDespawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDespawner : MonoBehaviour {
+
+    public float DespawnTime = 7.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        Invoke("DestroyGameObject", DespawnTime);
+	}
+	
+    public void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+}
