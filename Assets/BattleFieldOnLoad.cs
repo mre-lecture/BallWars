773fcad85a000259809e204c9b48a4e98c9c3b06
xrounder Miguel Caballero <1414163@stud.hs-mannheim.de>
2017-05-29 16:27:16 +0200

    added ball selection in menu and dynamic cheering sound
    added dynamic cheering sound

--- Assets/BattleFieldOnLoad.cs
+++ Assets/BattleFieldOnLoad.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BattleFieldOnLoad : MonoBehaviour {
 
     [SerializeField] private AudioClip game_music;
+    private GameObject[] audiences;
+    private GameObject goalball;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,29 @@ public class BattleFieldOnLoad : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        goalball = GameObject.Find("Goalball");
+        Transform ball = goalball.GetComponent<Transform>();
+
+        audiences = GameObject.FindGameObjectsWithTag("AudienceAudio");
+        foreach (GameObject audience in audiences)
+        {
+            AudioSource source = audience.GetComponent<AudioSource>();
+            if (source != null && ball != null)
+            {
+                //distance in percentage from 0 to 1
+                float distanceFromCenter = System.Math.Abs(ball.position.x/4);
+                //minVolume
+                if (distanceFromCenter <= 0.3)
+                {
+                    source.volume = 0.3f;
+                }else
+                {
+                    source.volume = distanceFromCenter;
+                }
+            }else
+            {
+                print("Audio Source or Goalball is null");
+            }
+        }
+    }
 }
