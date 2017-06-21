6593eac80344a5f51c61c179031363f0cffbc8ce
CHOCOLATENUTS\Admin <maxsoest@gmail.com>
2017-06-21 14:18:11 +0200

    Avatar with layer

---
+++ Assets/TennisBallSyncManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TennisBallSyncManager : Photon.MonoBehaviour {
+
+    bool isThrowing = false;
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (isThrowing)
+        {
+            if (stream.isWriting)
+            {
+                stream.SendNext(gameObject.transform.rotation);
+                stream.SendNext(gameObject.transform.position);
+            }
+            else
+            {
+                gameObject.transform.rotation = (Quaternion)stream.ReceiveNext();
+                gameObject.transform.position = (Vector3)stream.ReceiveNext();
+            }
+        }
+    }
+}
